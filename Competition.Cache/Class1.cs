72e26b634f766ea8b1a4e4b984c6efe82df846c6
liwenzan <liwenzan@DESKTOP-RD9V7FN>
2019-03-11 16:20:45 +0800

    test

--- Competition.Cache/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Competition.Cache
-{
-    public class Class1
-    {
-    }
-}
---
+++ Competition.Cache/ICache.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Competition.Cache
+{
+    public interface ICache
+    {
+        string Name { get; }
+        TimeSpan DefaultSlidingExpireTime { get; set; }
+        TimeSpan? DefaultAbsoluteExpireTime { get; set; }
+        object Get(string key, Func<string, object> factory);
+        object[] Get(string[] keys, Func<string, object> factory);
+        Task<object> GetAsync(string key, Func<string, Task<object>> factory);
+    }
+}
