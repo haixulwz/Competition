cfa4f00bec6dbf423c7a218937e2f1c1147f03e6
liwenzan <liwenzan@DESKTOP-RD9V7FN>
2019-03-12 14:02:49 +0800

    授权

--- Competition.Host/Controllers/UserLoginApiController.cs
+++ Competition.Host/Controllers/UserLoginApiController.cs
@@ -30,10 +30,27 @@ namespace Competition.Host.Controllers
         {
             var user = _store.FindUser(userDto.UserName, userDto.Password);
             if (user == null) return Unauthorized();
+            var authTime = DateTime.UtcNow;
+            var expiresAt = authTime.AddSeconds(double.Parse(_configuration["Authentication:JwtBearer:Expiration"]));
+            var tokenString = CreateAccessToken(user,expiresAt);
+            return Ok(new
+            {
+                access_token = tokenString,
+                token_type = "Bearer",
+                profile = new
+                {
+                    sid = user.Id,
+                    name = user.Name,
+                    auth_time = new DateTimeOffset(authTime).ToUnixTimeSeconds(),
+                    expires_at = new DateTimeOffset(expiresAt).ToUnixTimeSeconds()
+                }
+            });
+        }
+        private string CreateAccessToken(User user,DateTime? expiresAt)
+        {
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(_configuration["Authentication:JwtBearer:SecurityKey"]);
-            var authTime = DateTime.UtcNow;
-            var expiresAt = authTime.AddDays(7);
+            
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(new Claim[]
@@ -49,19 +66,7 @@ namespace Competition.Host.Controllers
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
             };
             var token = tokenHandler.CreateToken(tokenDescriptor);
-            var tokenString = tokenHandler.WriteToken(token);
-            return Ok(new
-            {
-                access_token = tokenString,
-                token_type = "Bearer",
-                profile = new
-                {
-                    sid = user.Id,
-                    name = user.Name,
-                    auth_time = new DateTimeOffset(authTime).ToUnixTimeSeconds(),
-                    expires_at = new DateTimeOffset(expiresAt).ToUnixTimeSeconds()
-                }
-            });
+            return   tokenHandler.WriteToken(token);
         }
 
     }
