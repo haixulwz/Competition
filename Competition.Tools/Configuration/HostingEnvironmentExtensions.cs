ae22b7a796a3fb29c55a5c134168e3c58cee3585
liwenzan <liwenzan@DESKTOP-RD9V7FN>
2019-03-12 17:41:53 +0800

    提交

---
+++ Competition.Tools/Configuration/HostingEnvironmentExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Competition.Tools.Configuration
+{
+  public static  class HostingEnvironmentExtensions
+    {
+    }
+}
