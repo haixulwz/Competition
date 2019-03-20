d3734194fdd9bcae87d18d7cc176e72a526920ad
liwenzan <liwenzan@DESKTOP-RD9V7FN>
2019-03-20 08:17:59 +0800

    connection

--- Competition.DbHelper/DalType.cs
+++ Competition.DbHelper/DalType.cs
@@ -26,4 +26,25 @@ namespace Competition.DbHelper
         /// </summary>
         SQLite  
     }
+    public static class DalTypeHelper
+    {
+        public static DalType GetDalType(string providerName)
+        {
+            switch (providerName)
+            {
+                case "System.Data.SqlClient":
+                    return DalType.MsSql;
+                case "System.Data.OracleClient":
+                    return DalType.Oracle;
+                case "System.Data.SQLite":
+                    return DalType.SQLite;
+                case "MySql.Data.MySqlClient":
+                    return DalType.MySql;
+                default:
+                    return DalType.MySql;
+            }
+        }
+    }
+
 }
+ 
--- Sample.lib2/ConnectionFactory.cs
+++ Sample.lib2/ConnectionFactory.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using Competition.DbHelper;
+using Competition.Tools;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace Sample.lib2
@@ -9,7 +15,34 @@ namespace Sample.lib2
     {
         public IDbConnection GetConnection()
         {
-            throw new NotImplementedException();
+            var config = ServiceLocator.Instance.GetService(typeof(IConfiguration)) as IConfiguration;
+            var connSetting = config["ConnectionStrings:Default"];
+            if (connSetting != null)
+            {
+                DbConnection connection = null;
+                var dalType = DalTypeHelper.GetDalType(config["ConnectionStrings:DataType"]);
+                switch (dalType)
+                {
+                    case DalType.MsSql:
+                        connection = new SqlConnection(connSetting);
+                        break;
+                    case DalType.Oracle:
+                        break;
+                    case DalType.MySql:
+                        connection = new MySqlConnection(connSetting);
+                        break;
+                    case DalType.SQLite:
+                        throw new Exception("not support SQLite");
+                    default:
+                        throw new Exception("not support");
+                }
+
+              
+                return connection;
+            }
+
+            throw new Exception($"{connSetting} not exist");
+           
         }
     }
 }
