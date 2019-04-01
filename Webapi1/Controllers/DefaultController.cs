5efbafede0ca2cccfa22328713e2ef1833c97d23
liwenzan <liwenzan@DESKTOP-RD9V7FN>
2019-04-01 19:09:03 +0800

    test

---
+++ Webapi1/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Webapi1.Model;
+ 
+namespace Webapi1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpPost]
+        public JsonResult PostPerson(Person p)
+        {
+            
+            return new JsonResult(p);
+        }
+    }
+}
---
+++ Webapi1/Model/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webapi1.Model
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+    }
+}
