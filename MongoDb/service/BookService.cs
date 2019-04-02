e078564df1478368e9b206fa54428739f0159884
liwenzan <liwenzan@DESKTOP-RD9V7FN>
2019-04-02 18:30:42 +0800

    book

---
+++ MongoDb/service/BookService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDb.Model;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoDb.service
+{
+    public class BookService
+    {
+        private readonly IMongoCollection<Book> _book;
+        public BookService(IConfiguration configuration)
+        {
+            var client = new MongoClient( configuration.GetConnectionString("BookstoreDb"));
+            var database = client.GetDatabase("BookstoreDb");
+            _book = database.GetCollection<Book>("Books");
+
+        }
+        public List<Book> Get()
+        {
+            return _book.Find(b=>true).ToList();
+        }
+
+        public Book Get(string id)
+        {
+            return _book.Find<Book>(book => book.Id == id).FirstOrDefault();
+        }
+        public Book Create(Book book)
+        {
+            _book.InsertOne(book);
+            return book;
+        }
+
+        public void Update(string id, Book bookIn)
+        {
+            _book.ReplaceOne(book => book.Id == id, bookIn);
+        }
+
+        public void Remove(Book bookIn)
+        {
+            _book.DeleteOne(book => book.Id == bookIn.Id);
+        }
+    }
+}
