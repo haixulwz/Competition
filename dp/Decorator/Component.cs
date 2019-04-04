a39ee264857420508aa4416353801ef3bf354b8a
liwenzan <liwenzan@DESKTOP-RD9V7FN>
2019-04-04 14:53:53 +0800

    啊

---
+++ dp/Decorator/Component.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dp.Decorator
+{
+   abstract public class Component
+    {
+        public abstract void Operation();
+    }
+}
---
+++ dp/Decorator/ConcreteComponent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dp.Decorator
+{
+    public class ConcreteComponent : Component
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("具体的组件");
+        }
+    }
+}
